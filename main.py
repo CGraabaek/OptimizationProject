34836f9b97584f32cdc844a1b3059c98bda0db89
Christian Graabaek <c.graabaek@gmail.com>
2017-11-07 14:01:58 +0100

    Split up into files.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import scipy.io as sio
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from mnist import MNIST
 from sklearn.neighbors import NearestCentroid
 from sklearn.model_selection import train_test_split
+import nearest_centroid_classifiers
 
 print('---------------------ORL - Nearest Centroid---------------------')
 orl_data = np.array(sio.loadmat('orl_data.mat')['data']).transpose()
@@ -12,35 +14,16 @@ orl_labels = np.array(sio.loadmat('orl_lbls.mat')['lbls']).ravel()
 
 orl_data_train, orl_data_test, orl_lables_train, orl_labels_test = train_test_split(orl_data,orl_labels,test_size=0.30,random_state=42)
 
-clf = NearestCentroid()
-# Train with the data
-clf.fit(orl_data_train,orl_lables_train)
+orl_score = nearest_centroid_classifiers.nc_fit(orl_data_train,orl_data_test,orl_lables_train,orl_labels_test)
 
-# Create predition for train data
-y_pred_test = clf.predict(orl_data_test)
+print('Score from function ' +str(orl_score))
 
-#How well does it fit
-score = clf.score(orl_data_test,orl_labels_test)
-
-print(score)
 
 print('---------------------MINST - Nearest Centroid---------------------')
-mndata = MNIST('./')
+mndata = MNIST('./', return_type='numpy')
 images_training, labels_training = mndata.load_training()
 images_test, labels_test = mndata.load_testing()
 
+minst_score = nearest_centroid_classifiers.nc_fit(images_training,images_test,labels_training,labels_test)
 
-img_train = mndata.process_images_to_numpy(images_training)
-img_test = mndata.process_images_to_numpy(images_test)
-lbl_train = mndata.process_images_to_numpy(labels_training)
-lbl_test = mndata.process_images_to_numpy(labels_test)
-
-# Train with the data
-clf.fit(img_train,lbl_train)
-
-# Create predition for train data
-labels_pred_test = clf.predict(img_test)
-
-#How well does it fit
-score_minst = clf.score(img_test,lbl_test)
-print(score_minst)
+print('Score from function ' +str(minst_score))
---
+++ nearest_centroid_classifiers.py
@@ -0,0 +1,26 @@
+import numpy as np
+import scipy.io as sio
+from sklearn.decomposition import PCA
+from mnist import MNIST
+from sklearn.neighbors import NearestCentroid
+
+
+def nc_fit(Xtrain,Xtest,Xtrain_lbls,Xtest_lbls):
+    clf = NearestCentroid()
+    # Train with the data
+    clf.fit(Xtrain,Xtrain_lbls)
+
+    # Create predition for train data
+    y_pred_test = clf.predict(Xtest)
+
+    #How well does it fit
+    score = clf.score(Xtest,Xtest_lbls)
+    return score
+
+
+def pca_fit(Xtrain,Xtest, components):
+    pca = PCA(n_components=components)
+    PCA_train = pca.fit_transform(Xtrain)
+    PCA_test = pca.fit(Xtrain).transform(Xtest)
+    return PCA_test
+
