c86ab709cccdeb988a3ffa1e30d2b0e5ca230e19
Christian Graabaek <c.graabaek@gmail.com>
2017-11-07 09:43:44 +0100

    Works for OCL

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import numpy as np
 import scipy.io as sio
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 from mnist import MNIST
 from sklearn.neighbors import NearestCentroid
 from sklearn.model_selection import train_test_split
 
-mndata = MNIST('./')
-images, labels = mndata.load_training()
-
-
+print('---------------------ORL - Nearest Centroid---------------------')
 orl_data = np.array(sio.loadmat('orl_data.mat')['data']).transpose()
 orl_labels = np.array(sio.loadmat('orl_lbls.mat')['lbls']).ravel()
 
-
-X_train, X_test = train_test_split(orl_data, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(orl_data,orl_labels,test_size=0.30,random_state=42)
 
 clf = NearestCentroid()
-clf.fit(orl_data, orl_labels)
+# Train with the data
+clf.fit(X_train,Y_train)
 
-#Z = clf.predict([orl_data.ravel(), orl_labels])
-y_pred = clf.predict(orl_data)
-print(y_pred)
+# Create predition for train data
+y_pred_test = clf.predict(X_test)
 
-diff = np.mean(orl_labels-y_pred)
-print(diff)
+#How well does it fit
+score = clf.score(X_test,Y_test)
 
+print(score)
+
+print('---------------------MINST - Nearest Centroid---------------------')
+mndata = MNIST('./')
+images, labels = mndata.load_training()
