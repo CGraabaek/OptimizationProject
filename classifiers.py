ac9823c49b4a3c6fe9906d647470c8df42f855aa
Christian Graabaek <cgs@syspeople.dk>
2017-12-07 16:51:58 +0100

    Added perceptron

--- classifiers.py
+++ classifiers.py
@@ -8,78 +8,170 @@ import numpy as np
 from scipy.stats import mode
 from sklearn.metrics import pairwise_distances_argmin
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import add_dummy_feature
 from time import time
 
 
-def nc_fit(Xtrain,Xtest,Xtrain_lbls,Xtest_lbls,name,data):
-    t0 = time()
+def nc_fit(Xtrain, Xtest, Xtrain_lbls, Xtest_lbls, name, data, t0=time()):
     clf = NearestCentroid()
     # Train with the data
-    clf.fit(Xtrain,Xtrain_lbls)
+    clf.fit(Xtrain, Xtrain_lbls)
 
-    # Create predition for train data
+    # Create prediction for train data
     y_pred_test = clf.predict(Xtest)
 
-    #How well does it fit
-    score = clf.score(Xtest,Xtest_lbls)
+    # How well does it fit
+    score = clf.score(Xtest, Xtest_lbls)
+
+    print('%-9s\t\t%.2fs\t%-9s\t%-9s'
+          % (name, (time() - t0), score, data))
+
+    # return score, y_pred_test
 
 
-  
-
-    print('%-9s\t%.2fs\t%i\t%-9s'
-          % (name, (time()-t0),score,data))
-
-    ##return score, y_pred_test
-
-def pca_fit(Xtrain,Xtest,components):
+def pca_fit(Xtrain, Xtest, components):
     pca = PCA(n_components=components)
-    PCA_train = pca.fit_transform(Xtrain)
-   # PCA_test = pca.fit(Xtrain).transform(Xtest)
-    PCA_test = pca.transform(Xtest)
+    pca_train = pca.fit_transform(Xtrain)
+    pca_test = pca.fit(Xtrain).transform(Xtest)
 
-    return PCA_train, PCA_test
+    return pca_train, pca_test
 
-def nn_fit(Xtrain,Xtest,Xtrain_lbls,Xtest_lbls):
+
+def nn_fit(Xtrain, Xtest, Xtrain_lbls, Xtest_lbls, name, data, t0=time()):
     clf = neighbors.KNeighborsClassifier(1)
     clf.fit(Xtrain, Xtrain_lbls)
     Z = clf.predict(Xtest)
-    score = clf.score(Xtest,Xtest_lbls) 
-    return score, Z
+    score = clf.score(Xtest, Xtest_lbls)
+
+    print('%-9s\t\t%.2fs\t%-9s\t%-9s'
+          % (name, (time() - t0), score, data))
+    # return score, Z
 
 
-def nsc_fit(Xtrain,Xtrain_lbls,Xtest,Xtest_lbls,n_clust,rng):
+def nsc_fit(Xtrain, Xtrain_lbls, Xtest, Xtest_lbls, n_clust, rng, name, datat, t0=time()):
     centers = []
     labels = []
-    new_labels = []
     correct_labels = []
 
-    for i in range(0,rng):
-        data = Xtrain[np.nonzero(Xtrain_lbls==i)]
-        kmeans = KMeans(n_clusters=n_clust,random_state=42).fit(data)
+    for i in range(0, rng):
+        data = Xtrain[np.nonzero(Xtrain_lbls == i)]
+        kmeans = KMeans(n_clusters=n_clust, random_state=42).fit(data)
 
-        for k in range(0,n_clust):
-            centers.append(kmeans.cluster_centers_[k,:])
-            labels.append(str(i) +  '_' + str(k))
- 
+        for k in range(0, n_clust):
+            centers.append(kmeans.cluster_centers_[k, :])
+            labels.append(str(i) + '_' + str(k))
+
     clf = NearestCentroid()
+    clf.fit(centers, labels)
+    pred = clf.predict(Xtest)
+
+    for i in range(0, len(pred)):
+        correct_labels.append(int(pred[i].split('_')[0]))
+
+    score = accuracy_score(Xtest_lbls, correct_labels)
+
+    print('%-9s\t\t%.2fs\t%-9s\t%-9s'
+          % (name, (time() - t0), score, datat))
+    # return score
 
 
-    clf.fit(centers,labels)
-    predict = clf.predict(Xtest)
+def perceptron_sgd_plot(X, Y, LookFor):
+    '''
+    train perceptron and plot the total loss in each epoch.
 
-    for i in range(0,len(predict)):
-       correct_labels.append(int(predict[i].split('_')[0]))
+    :param X: data samples
+    :param Y: data labels
+    :return: weight vector as a numpy array
+    '''
+    X = add_dummy_feature(X)
 
-    score = accuracy_score(Xtest_lbls,correct_labels)
+    w = np.zeros(len(X[0]))
+    eta = 0.1
+    n = 100
+    errors = []
+    bdata = []
+
+    for t in range(n):
+        total_error = 0
+        tot = 0
+        for i, x in enumerate(X):
+            if(Y[i] == LookFor):
+                y = 1
+            else:
+                y = -1
+
+            if (np.dot(X[i], w) * Y[i]) <= 0:
+                #w = w + eta * X[i] * Y[i]
+                total_error += X[i] * y
+
+        w += total_error*eta
+
+        errors.append(tot)
+
+   # print(bdata)
+   # plt.plot(errors)
+   # plt.xlabel('Iterations')
+   # plt.ylabel('Total Errors')
+   # plt.show()
+
+    return w
+
+# Make a prediction with weights
+
+def predict(w, xtest, xtest_labels):
+    k = np.asarray(w)
     
+
+    test = add_dummy_feature(xtest).transpose()
+    decision = np.dot(w,test)
+    classification = np.argmax(decision,axis=0)
+
+    try:
+        score = accuracy_score(xtest_labels,classification)
+    except ValueError:
+        score = 0
     return score
 
 
-def cluster(X) :
-    kmeans = KMeans(n_clusters = 3, random_state = 0)                   
-    kmeans.fit(X)                  
-    plt.scatter(X[:, 0], X[:, 1],c = kmeans.labels_)
-    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 50, c = 'yellow') 
+def predict2(row, weights):
+    activation = weights[0]
+    for i in range(len(row) - 1):
+        activation += weights[i + 1] * row[i]
+    return 1.0 if activation >= 0.0 else 0.0
+
+# Estimate Perceptron weights using stochastic gradient descent
+
+
+def train_weights(train, l_rate, n_epoch):
+    weights = [0.0 for i in range(len(train[0]))]
+    errors = []
+    for epoch in range(n_epoch):
+        sum_error = 0.0
+        for row in train:
+            prediction = predict2(row, weights)
+            error = row[-1] - prediction
+            sum_error += error**2
+            weights[0] = weights[0] + l_rate * error
+            for i in range(len(row) - 1):
+                weights[i + 1] = weights[i + 1] + l_rate * error * row[i]
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+        errors.append(sum_error)
+
+
+    plt.plot(errors)
+    plt.xlabel('Iterations')
+    plt.ylabel('Total Loss')
     plt.show()
+    return weights
 
 
+def binary_classifyer (XLables,wanted_label):
+    newLabes = []
+
+    for i, e in enumerate(XLables):
+        if(int(e) == int(wanted_label)):
+            newLabes.append(1)
+        else:
+            newLabes.append(-1)
+    
+    return newLabes
--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from time import time
 #ORL
 orl_data = np.array(sio.loadmat('orl_data.mat')['data']).transpose()
 orl_labels = np.array(sio.loadmat('orl_lbls.mat')['lbls']).ravel()
-orl_data_train, orl_data_test, orl_lables_train, orl_labels_test = train_test_split(orl_data,orl_labels,test_size=0.30,random_state=42)
+orl_data_train, orl_data_test, orl_lables_train, orl_labels_test = train_test_split(orl_data,orl_labels,test_size=0.30,random_state=42,stratify=orl_labels)
 orl_pca_train, orl_pca_test = classifiers.pca_fit(orl_data_train,orl_data_test,2)
 orl_pca_train_10, orl_pca_test_10 = classifiers.pca_fit(orl_data_train,orl_data_test,10)
 #MINST
@@ -27,46 +27,53 @@ images_test, labels_test = mndata.load_testing()
 
 minst_pca_train, minst_pca_test = classifiers.pca_fit(images_training,images_test,2)
 minst_pca_train_10, minst_pca_test_10 = classifiers.pca_fit(images_training,images_test,10)
+'''
+print(82 * '_')
+print("Classifiers")
+print(82 * '_')
+print('name\t\t\t\ttime\tscore\t\tdata')
 
+classifiers.nc_fit(orl_data_train,orl_data_test,orl_lables_train,orl_labels_test, "Nearest Centroid", "ORL")
+classifiers.nc_fit(images_training,images_test,labels_training,labels_test,"Nearest Centroid","MNIST")
+#classifiers.nn_fit(orl_data_train,orl_data_test,orl_lables_train,orl_labels_test,"Nearest Neighbor","ORL")
+#classifiers.nn_fit(images_training,images_test,labels_training,labels_test, "NN","MNIST")
+classifiers.nsc_fit(images_training,labels_training,images_test,labels_test,2,10,"NSC - 2 Clusters","MNIST")
+#classifiers.nsc_fit(orl_data_train,orl_data_test,orl_lables_train,orl_labels_test,2,10,"NSC - 2 Clusters","ORL")
+classifiers.nsc_fit(images_training,labels_training,images_test,labels_test,3,10,"NSC - 3 Clusters","MNIST")
+classifiers.nsc_fit(images_training,labels_training,images_test,labels_test,5,10,"NSC - 5 Clusters","MNIST")
 
 print(82 * '_')
-print('name\t\ttime\tscore\tdata')
+print("PCA - 2 Components")
+print(82 * '_')
+classifiers.nc_fit(orl_pca_train,orl_pca_test,orl_lables_train,orl_labels_test, "Nearest Centroid - PCA 2","ORL")
+classifiers.nc_fit(minst_pca_train,minst_pca_test,labels_training,labels_test,"Nearest Centroid - PCA 2","MNIST")
+#classifiers.nn_fit(orl_pca_train,orl_pca_test,orl_lables_train,orl_labels_test,"Nearest Neighbor - PCA 2","ORL")
+#classifiers.nn_fit(minst_pca_train,minst_pca_test,labels_training,labels_test, "NN - PCA 2 ","MNIST")
+classifiers.nsc_fit(minst_pca_train,labels_training,minst_pca_test,labels_test,2,10,"NSC - 2 Clusters - PCA 2","MNIST")
+classifiers.nsc_fit(minst_pca_train,labels_training,minst_pca_test,labels_test,3,10,"NSC - 3 Clusters - PCA 2","MNIST")
+classifiers.nsc_fit(minst_pca_train,labels_training,minst_pca_test,labels_test,5,10,"NSC - 5 Clusters - PCA 2","MNIST")
 
-#print('--------------------- Nearest Centroid - ORL ---------------------')
-#orl_score,orl_score_predicted_labels = classifiers.nc_fit(orl_data_train,orl_data_test,orl_lables_train,orl_labels_test)
-#orl_pca_score_2,orl_pca_pred_labels2 = classifiers.nc_fit(orl_pca_train,orl_pca_test,orl_lables_train,orl_labels_test)
-#orl_pca_score_10,orl_pca_pred_labels10 = classifiers.nc_fit(orl_pca_train_10,orl_pca_test_10,orl_lables_train,orl_labels_test)
-#print('Score from function ' + str(orl_score))
-#print('Score from PCA - 2 Components ' + str(orl_pca_score_2))
-#print('Score from PCA - 10 Components ' + str(orl_pca_score_10))
+print(82 * '_')
+print("PCA - 10 Components")
+print(82 * '_')
+classifiers.nc_fit(orl_pca_train_10,orl_pca_test_10,orl_lables_train,orl_labels_test,"Nearest Centroid  - PCA 10","ORL")
+classifiers.nc_fit(minst_pca_train_10,minst_pca_test_10,labels_training,labels_test,"Nearest Centroid  - PCA 10","MNIST")
+#classifiers.nn_fit(orl_pca_train_10,orl_pca_test_10,orl_lables_train,orl_labels_test,"Nearest Neighbor - PCA 10", "ORL")
+#classifiers.nn_fit(minst_pca_train_10,minst_pca_test_10,labels_training,labels_test,"NN - PCA 10","MNIST")
+classifiers.nsc_fit(minst_pca_train,labels_training,minst_pca_test,labels_test,2,10,"NSC - 2 Clusters - PCA 10","MNIST")
+classifiers.nsc_fit(minst_pca_train,labels_training,minst_pca_test,labels_test,3,10,"NSC - 3 Clusters - PCA 10","MNIST")
+classifiers.nsc_fit(minst_pca_train,labels_training,minst_pca_test,labels_test,5,10,"NSC - 5 Clusters - PCA 10","MNIST")
 
-classifiers.nc_fit(orl_data_train,orl_data_test,orl_lables_train,orl_labels_test, "NC", "ORL")
-classifiers.nc_fit(orl_pca_train,orl_pca_test,orl_lables_train,orl_labels_test, "NC - PCA 2","ORL")
-classifiers.nc_fit(orl_pca_train_10,orl_pca_test_10,orl_lables_train,orl_labels_test,"NC - PCA 10","ORL")
+'''
+
+
+##---------------------Nearest Centroid----------------------------------------
 
 
 #plot.plot_data(orl_pca_test,orl_labels_test,"NC ORL Test Data")
 #plot.plot_data(orl_pca_test,orl_pca_pred_labels2,"NC Classified ORL Test Data")
 #plot.plot_orl_n_by_m(orl_data_train,10,28)
 #plot.plot_orl_face(orl_data_train)
-
-
-
-
-#print('--------------------- Nearest Centroid - MINST ---------------------')
-
-#minst_score,minst_score_predicted_labels = classifiers.nc_fit(images_training,images_test,labels_training,labels_test)
-#minst_pca_score_2,minst_pca_pred_labels2 = classifiers.nc_fit(minst_pca_train,minst_pca_test,labels_training,labels_test)
-#minst_pca_score_10,minst_pca_pred_labels10 = classifiers.nc_fit(minst_pca_train_10,minst_pca_test_10,labels_training,labels_test)
-
-#print('Score from function ' + str(minst_score))
-#print('Score from PCA - 2 Components ' + str(minst_pca_score_2))
-#print('Score from PCA - 10 Components ' + str(minst_pca_score_10))
-
-classifiers.nc_fit(images_training,images_test,labels_training,labels_test,"NC","MNIST")
-classifiers.nc_fit(minst_pca_train,minst_pca_test,labels_training,labels_test,"NC PCA 2","MNIST")
-classifiers.nc_fit(minst_pca_train_10,minst_pca_test_10,labels_training,labels_test,"NC PCA 10","MNIST")
-
 
 #plot.plot_data(minst_pca_test,labels_test,"NC MINST Test Data",1)
 #plot.plot_data(minst_pca_test,minst_pca_pred_labels2,"NC Classified MINST Test Data",2)
@@ -82,33 +89,19 @@ classifiers.nc_fit(minst_pca_train_10,minst_pca_test_10,labels_training,labels_t
 
 ##print(str(labels_test[4]))
 
+#print('--------------------- Nearest Neighbor - ORL ---------------------')
 
+
+#plot.plot_data(orl_pca_test,orl_labels_test,"NN ORL Test Data")
+#plot.plot_data(orl_pca_test,nn_orl_pca_pred_labels2,"NN Classified ORL Test Data")
 '''
-print('--------------------- Nearest Neighbor - ORL ---------------------')
-
-orl_score_nn,nn_score_pred = classifiers.nn_fit(orl_data_train,orl_data_test,orl_lables_train,orl_labels_test)
-orl_pca_nn_score_2,nn_orl_pca_pred_labels2 = classifiers.nn_fit(orl_pca_train,orl_pca_test,orl_lables_train,orl_labels_test)
-orl_pca_nn_score_10,nn_orl_pca_pred_labels10 = classifiers.nn_fit(orl_pca_train_10,orl_pca_test_10,orl_lables_train,orl_labels_test)
-print('Score from function ' + str(orl_score_nn))
-print('Score from PCA - 2 Components ' + str(orl_pca_nn_score_2))
-print('Score from PCA - 10 Components ' + str(orl_pca_nn_score_10))
-
-plot.plot_data(orl_pca_test,orl_labels_test,"NN ORL Test Data")
-plot.plot_data(orl_pca_test,nn_orl_pca_pred_labels2,"NN Classified ORL Test Data")
-
 print('--------------------- Nearest Neighbor - MNIST ---------------------') 
-#minst_score_nn,_minst_pred_score_nn = classifiers.nn_fit(images_training,images_test,labels_training,labels_test)
-minst_pca_score_2_nn,minst_nn_pca_pred_labels2 = classifiers.nn_fit(minst_pca_train,minst_pca_test,labels_training,labels_test)
-minst_pca_score_10_nn,minst_nn_pca_pred_labels10 = classifiers.nn_fit(minst_pca_train_10,minst_pca_test_10,labels_training,labels_test)
-
 #print('Score from function ' + str(minst_score_nn))
 print('Score from PCA - 2 Components ' + str(minst_pca_score_2_nn))
 print('Score from PCA - 10 Components ' + str(minst_pca_score_10_nn))
 
 plot.plot_data(minst_pca_test,labels_test,"NN MINST Test Data",1)
 plot.plot_data(minst_pca_test,minst_nn_pca_pred_labels2,"NN Classified MINST Test Data",2)
-
-
 
 print('--------------------- Nearest Sub Class - ORL ---------------------')
 
@@ -134,3 +127,29 @@ print('Score from function NSC - 2 clusters ' + str(nsc_score2_mnist))
 print('Score from function NSC - 3 clusters ' + str(nsc_score3_mnist))
 print('Score from function NSC - 5 clusters ' + str(nsc_score5_mnist))
 '''
+
+print('--------------------- Batch Perceptron ---------------------')
+
+#new_lbls = classifiers.binary_classifyer(labels_training,2)
+#print(new_lbls)
+
+weight_array = []
+for i in range(0,10):
+    new_lbls = classifiers.binary_classifyer(labels_training,i)
+    weight_array.append(classifiers.perceptron_sgd_plot(images_training,new_lbls,i))
+   #np.dot(w, xtest[i])
+#print(weight_array)
+
+score = classifiers.predict(weight_array,images_test,labels_test)
+print(score)
+#     labels.append(str(i))
+
+#     for k in range(0, n_clust):
+#         centers.append(kmeans.cluster_centers_[k, :])
+
+# for i in range(0, 10):
+#     weight_array.append(classifiers.perceptron_sgd_plot(X,y))
+
+# data = Xtrain[np.nonzero(Xtrain_lbls == 0)]
+
+#print(classifiers.perceptron_sgd_plot(X,y))
